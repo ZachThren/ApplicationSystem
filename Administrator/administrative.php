2f53d8c8fe0f9717b78421c9003b9f98226b7dd7
Tumasang Che <tche1@terpmail.umd.edu>
2018-04-03 10:40:27 -0400

    few changes
    administrative.php

--- Administrator/administrative.php
+++ Administrator/administrative.php
@@ -7,13 +7,13 @@ if(isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW']) &&
     $_SERVER['PHP_AUTH_USER'] === "main" && $_SERVER['PHP_AUTH_PW'] === "terps"){
 
     $body = <<<EOBODY
-        <form action="{$_SERVER["PHP_SELF"]}" method="post" class="container-fluid">
-        <br><img src="umdLogo.gif" alt="UMD logo"/><br>
-        <hr style="height:1px;border:none;color:#333;background-color:#333;" />
-        <h1>Applications</h1><br>
-        
-        <strong>Select Course</strong><br>                
-        <select name="course" class="container-fluid">                                       
+            <form action="{$_SERVER["PHP_SELF"]}" method="post" class="container-fluid">
+            <br><img src="umdLogo.gif" alt="UMD logo"/><br>
+            <hr style="height:1px;border:none;color:#333;background-color:#333;" />
+            <h1><b>Applications</b></h1><br>
+            
+            <h4><b>Select Course</b></h4>
+            <select name="course" class="container-fluid">                                       
 EOBODY;
     
     foreach ($courses as $course) {
@@ -21,31 +21,28 @@ EOBODY;
     }
 
     $body .= <<<eobody
-        </select><br><br>
-
-        <strong>Select field to sort applications</strong>
-        <select name="sortby" class="container-fluid">
-            <option value="select">[select]</option>
-            <option value="uid">University ID</option>
-            <option value="name" selected>name</option>
-            <option value="email">email</option>
-            <option value="gpa">gpa</option>
-            <option value="year">year</option>
-            <option value="gender">gender</option>
-        </select><br><br>
-        
-        <input type="radio" name="graduate" id="graduate" value="graduate"/><label for="graduate">Graduate</label>
-        <input type="radio" name="graduate" id="undergrad" value="undergrad" /><label for="undergrad">Undergraduate</label><br><br>
-        
-        <strong>Filter Condition (optional)</strong>
-        <input type="text" name="filter" style="background-color: lavender"><br><br>
-        
-        <input type="submit" name="displayApp" value="Display Applications"/>
-        <br><br><hr style="height:1px;border:none;color:#333;background-color:#333;" />
-        <div style="text-align:left"> If you have any question about our program, please contact the system administrator at
-            <a style="text-align:center" href="mailto:your address" >tche1@terpmail.umd.edu</a>
-        </div>
-        </form>
+            </select ><br><br>
+    
+            <h4><b>Sort applications by<b></h4>
+            <select name="sortby" class="container-fluid">
+                <option value="select">[select]</option>
+                <option value="uid">University ID</option>
+                <option value="name" selected>name</option>
+                <option value="email">email</option>
+                <option value="gpa">gpa</option>
+                <option value="year">year</option>
+                <option value="gender">gender</option>
+            </select><br><br>
+            
+            <input type="radio" name="graduate" id="graduate" value="graduate"/><label class="choice" for="graduate">Graduate</label><br />
+            <input type="radio" name="graduate" id="undergrad" value="undergrad" /><label class="choice" for="undergrad">Undergraduate</label><br><br>
+            
+            <input type="submit" class="form-control" name="displayApp" value="Display Applications"/>
+            <br><br><hr style="height:1px;border:none;color:#333;background-color:#333;" />
+            <div style="text-align:left"> If you have any question about our program, please contact the system administrator at
+                <a style="text-align:center" href="mailto:your address" >tche1@terpmail.umd.edu</a>
+            </div>
+            </form><br>
 eobody;
 
 
--- Administrator/pager.php
+++ Administrator/pager.php
@@ -8,16 +8,18 @@ function generatePage($body, $title) {
                 <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title> $title </title> 
-                <link rel="stylesheet" href="mainstyle.css" type="text/css" />
+                <link rel="stylesheet" href="myStyles.css" type="text/css" />
                 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
             </head>
                     
-            <body class="container-fluid">
+            <body class="container-fluid" style="font-family: 'Courier New'">
                 $body
+                <script src="bootstrap/jquery-3.2.1.min.js"></script>
+                <script src="bootstrap/js/bootstrap.min.js"></script>
             </body>
         </html>
 EOPAGE;
 
-    return $page;
-}
+        return $page;
+    }
 ?>
