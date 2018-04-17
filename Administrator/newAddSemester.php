14825e60ec0381e3e234828c61be1082b9b795c8
Zach <Zach.Thren@gmail.com>
2018-04-17 01:28:27 -0400

    Adding to Administrative

--- Administrator/newAddSemester.php
+++ Administrator/newAddSemester.php
@@ -6,22 +6,85 @@
   if ($db->connect_error) {
     die($db->connect_error);
   } else {
-    $query = "select season, year from Semesters order by ID desc limit 1";
+    $query = "select * from Semesters order by ID desc limit 1";
+    $result = $db->query($query);
     $result->data_seek(0);
-    $actual_result = $result->fetch_array(MYSQLI_ASSOC);
-
-    if ($actual_result['season'] == "Winter") {
+    $result_array = $result->fetch_array(MYSQLI_ASSOC);
+    if ($result_array['Season'] == "Winter") {
       $this_season = "Spring";
-      $this_year = $actual_result['year'];
-    } else if ($actual_result['season'] == "Spring") {
+      $this_year = $result_array['Year'];
+      $past_year = (string)($this_year - 1);
+      $query = "select numOfCourses from Semesters where Season=\"Fall\" and Year=\"$past_year\"";
+      $result = $db->query($query);
+      $result->data_seek(0);
+      $result_array = $result->fetch_array(MYSQLI_ASSOC);
+      $courses = $result_array['numOfCourses'];
+      $query = "select Course, Max_Total from Courses_Fall_{$past_year}";
+    } else if ($result_array['Season'] == "Spring") {
       $this_season = "Summer";
-      $this_year = $actual_result['year'];
-    } else if ($actual_result['season'] == "Summer") {
+      $this_year = $result_array['Year'];
+      $past_year = (string)($this_year - 1);
+      $query = "select numOfCourses from Semesters where Season=\"Summer\" and Year=\"$past_year\"";
+      $result = $db->query($query);
+      $result->data_seek(0);
+      $result_array = $result->fetch_array(MYSQLI_ASSOC);
+      $courses = $result_array['numOfCourses'];
+      $query = "select Course, MaxTotal from Courses_Summer_{$past_year}";
+    } else if ($result_array['Season'] == "Summer") {
       $this_season = "Fall";
-      $this_year = $actual_result['year'];
+      $this_year = $result_array['Year'];
+      $query = "select numOfCourses from Semesters where Season=\"Spring\" and Year=\"$this_year\"";
+      $result = $db->query($query);
+      $result->data_seek(0);
+      $result_array = $result->fetch_array(MYSQLI_ASSOC);
+      $courses = $result_array['numOfCourses'];
+      $query = "select Course, MaxTotal from Courses_Spring_{$this_year}";
     } else {
       $this_season = "Winter";
-      $this_year = (string)($actual_result['year'] + 1);
+      $this_year = (string)($result_array['Year'] + 1);
+      $past_year = (string)($this_year - 1);
+      $query = "select numOfCourses from Semesters where Season=\"Winter\" and Year=\"$past_year\"";
+      $result = $db->query($query);
+      $result->data_seek(0);
+      $result_array = $result->fetch_array(MYSQLI_ASSOC);
+      $courses = $result_array['numOfCourses'];
+      $query = "select Course, MaxTotal from Courses_Winter_{$past_year}";
     }
+    $result = $db->query($query);
+    $body = <<<EOBODY
+      <form action="newAdministrative.php" method="post">
+        <div class="form-row">
+          <div class="col">
+            <strong>Season</strong></br>
+            <input type="text" class="form-control" name="season" value="$this_season"/>
+          </div>
+          <div class="col">
+            <strong>Year</strong></br>
+            <input type="text" class="form-control" name="year" value="$this_year"/>
+          </div>
+        </div>
+EOBODY;
+    for ($index = 1; $index <= $courses; $index++) {
+      $result->data_seek($index - 1);
+      $result_array = $result->fetch_array(MYSQLI_ASSOC);
+      $body .= <<<EOBODY
+        <div class="form-row">
+          <div class="col">
+            <strong>Course #$index</strong></br>
+            <input type="text" class="form-control" name="course$index" value="{$result_array['Course']}"/>
+          </div>
+          <div class="col">
+            <strong>Maximum TAs</strong></br>
+            <input type="number" class="form-control" name="maxTA$index" value="{$result_array['Max_Total']}"/>
+          </div>
+        </div>
+EOBODY;
+    }
+    $body .= <<<EOBODY
+        <br><input type="submit" class="form-control" value="Create Semester" name="submit"/><br>
+        <a class="btn btn-primary" href="newAdministrative.php" role="button">Go Back</a>
+      </form>
+EOBODY;
+    echo generatePage($body, "Add Semester");
   }
 ?>
--- Administrator/newAdministrative.php
+++ Administrator/newAdministrative.php
@@ -6,6 +6,50 @@
   if ($db->connect_error) {
     die($db->connect_error);
   } else {
+    if (isset($_POST["submit"])) {
+      $query = "create table testApplications_{$_POST["season"]}_{$_POST["year"]} (
+        First varchar(20),
+        Last varchar(20),
+        Email varchar(32),
+        Directory_ID varchar(32) primary key,
+        GPA float,
+        Courses varchar(200),
+        Previous varchar(300),
+        Degree enum('Undergraduate','MS','PhD'),
+        Transcript blob,
+        Position_Type enum('Full','Part'),
+        Want_Teach tinyint,
+        Advisor varchar(32),
+        Current_TA tinyint,
+        Current_Step enum('1','2','3'),
+        Current_Course varchar(32),
+        Current_Instructor varchar(32),
+        Passed_MEI tinyint,
+        Taking_UMEI tinyint,
+        Extra_Information varchar(500)
+      )";
+      $result = $db->query($query);
+      $query = "create table testCourses_{$_POST["season"]}_{$_POST["year"]} (
+        Course varchar(50) primary key,
+        Applying_Undergraduate varchar(1500),
+        Applying_Graduate varchar(1000),
+        Accepted_Undergraduate varchar(1500),
+        Accepted_Graduate varchar(1000),
+        Max_Undergraduate int,
+        Max_Graduate int,
+        Max_Total int
+      )";
+      $result = $db->query($query);
+      $total_courses = ((count($_POST) - 3) / 2);
+      for ($index = 1; $index <= $total_courses; $index++) {
+        $query = "insert into Courses_{$_POST["season"]}_{$_POST["year"]}
+          (Course, Max_Total) values (\"{$_POST["course$index"]}\", {$_POST["maxTA$index"]})";
+        $result = $db->query($query);
+      }
+      $query = "insert into Semesters (Season, Year, NumOfCourses) values
+        (\"{$_POST["season"]}\", \"{$_POST["year"]}\", $total_courses)";
+      $result = $db->query($query);
+    }
     $body = "<br><div id=\"accordion\">";
     $query = "select season, year from Semesters order by ID desc";
     $result = $db->query($query);
