54462fc35607db0be42500268e11e344df1e2a75
Erik Ochoa <erikochoa26051@gmail.com>
2018-04-25 10:54:51 -0400

    updated script to add students more than one at a time

--- Administrator/autofill.php
+++ Administrator/autofill.php
@@ -31,6 +31,8 @@
 
         $accepted_Undergraduate = [];
         $accepted_Graduate = [];
+        $new_Max_Ugrad = [];
+        $new_Max_Grad = []
 
         $result1 = mysqli_query($db_connection, $course_query);
         if (!$result1) {
@@ -52,7 +54,20 @@
             }
         }
 
-        $applications_query = "select Directory_ID, GPA from {$applicationsTable} order by GPA";
+        if (empty($applying_Undergraduate)) {
+            $applying_Undergraduate = [];
+        }
+        if (empty($applying_Graduate)) {
+            $applying_Graduate = [];
+        }
+        if (empty($accepted_Undergraduate)) {
+            $accepted_Undergraduate = [];
+        }
+        if (empty($accepted_Graduate)) {
+            $accepted_Graduate = [];
+        }
+
+        $applications_query = "select Directory_ID, GPA, Degree from {$applicationsTable} order by GPA";
 
         $result2 = mysqli_query($db_connection, $applications_query);
         if (!$result2) {
@@ -63,6 +78,31 @@
             if ($num_rows === 0) {
                 echo "No Applications<br>";
             } else {
+
+                $result2->data_seek($row_index);
+                $row = $result2->fetch_array(MYSQLI_ASSOC);
+
+                $addedUndergrad = 0;
+                $addedGrad = 0;
+
+                for ($row_index = 0; $row_index < $num_rows; $row_index++) {
+                    $result2->data_seek($row_index);
+                    $row = $result2->fetch_array(MYSQLI_ASSOC);
+
+                    if ($addedUndergrad >= $new_Max_Ugrad && $addedGrad >= $new_Max_Grad) {
+                        break;
+                    }
+
+                    if ($row["Degree"] == "Undergraduate") {
+                        array_push($accepted_Undergraduate, $row["Directory_ID"]);
+                        $addedUndergrad = $addedUndergrad + 1;
+                    } else {
+                        array_push($accepted_Graduate, $row["Directory_ID"]);
+                        $addedGrad = $addedGrad + 1;
+                    }
+                }
+
+                /*
                 while ($a_student = mysqli_fetch_array($result2, MYSQLI_ASSOC)) {
                     if ($a_student["GPA"] >= $gpa_cutoff && ($a_student["Degree"] === 'MS' ||  $a_student["Degree"] === 'PhD')) {
                         unset($applying_Graduate[$a_student["Directory_ID"]]);
@@ -74,72 +114,43 @@
                         $new_Max_Ugrad--;
                     }
                 }
+                */
             }
 
-            $update_Applying_Ugrad = serialize($applying_Undergraduate);
-            $update_Applying_Grad = serialize($applying_Graduate);
-            $update_Accepted_Ugrad = serialize($accepted_Undergraduate);
-            $update_Accepted_Grad = serialize($accepted_Graduate);
+            $updated_applying_u = [];
+            foreach($applying_Undergraduate as $key => $value) {
+                if (!(in_array($value, $accepted_Undergraduate))) {
+                    array_push($updated_applying_u, $value);
+                }
+            }
 
-            $update_query = "update {$applicationsTable}, set (Applying_Undergraduate = {$update_Applying_Ugrad}, Applying_Graduate = {$update_Applying_Grad}, Accepted_Undergraduate = {$update_Accepted_Ugrad}, Accepted_Graduate = {$update_Accepted_Grad}, Max_Undergraduate = {$new_Max_Ugrad}, Max_Graduate = {$new_Max_Grad}) where Course = '{$every_course}'";
-            mysqli_query($db_connection, $update_query);
+            $updated_applying_g = [];
+            foreach($applying_Graduate as $key => $value) {
+                if (!(in_array($value, $accepted_Graduate))) {
+                    array_push($updated_applying_g, $value);
+                }
+            }
+
+            $final_applying_undergraduate = serialize($updated_applying_u);
+            $final_applying_graduate = serialize($updated_applying_g);
+            $final_accepted_undergraduate = serialize($accepted_Undergraduate);
+            $final_accepted_graduate = serialize($accepted_Graduate);
+
+            $update_query = "update {$coursesTable} ";
+            $update_query .= "set Applying_Undergraduate = '".$final_applying_undergraduate."', Applying_Graduate = '".$final_applying_graduate."', Accepted_Undergraduate = '".$final_accepted_undergraduate."', Accepted_Graduate = '".$final_accepted_graduate."' ";
+            $update_query .= "where Course = '{$every_course}'";
+
+
+            $result = $db_connection->query($update_query);
+            if (!$result) {
+                die("Retrieval of courses failed: ". $db_connection->error);
+            } else {
+                echo "success";
+            }
         }
     }
-//    $course_query = "select Course, Applying_Undergraduate, Applying_Graduate, Accepted_Undergraduate, Accepted_Graduate, Max_Undergraduate, Max_Graduate, Max_Total from {$coursesTable} where Course = '{$course}'";
-//
-//    $applying_Undergraduate = [];
-//    $applying_Graduate = [];
-//
-//    $accepted_Undergraduate = [];
-//    $accepted_Graduate = [];
-//
-//    $result1 = $db_connection->query($course_query);
-//    if (!$result1) {
-//        die("Retrieval of courses failed: ". $db_connection->error);
-//    } else {
-//        $num_rows = $result1->num_rows;
-//        if ($num_rows === 0) {
-//            echo "Empty Table<br>";
-//        } else {
-//            $result1->data_seek(0);
-//            $row = $result1->fetch_array(MYSQLI_ASSOC);
-//            $applying_Undergraduate = unserialize($row["Applying_Undergraduate"]);
-//            $applying_Graduate = unserialize($row["Applying_Graduate"]);
-//
-//            $accepted_Undergraduate = unserialize($row["Accepted_Undergraduate"]);
-//            $accepted_Graduate = unserialize($row["Accepted_Graduate"]);
-//        }
-//    }
-//
-//    $applications_query = "select Directory_ID, GPA from {$applicationsTable} order by GPA";
-//
-//    $result2 = $db_connection->query($applications_query);
-//    if (!$result2) {
-//        die("Retrieval failed: ". $db_connection->error);
-//    } else {
-//        $num_rows = $result2->num_rows;
-//
-//        if ($num_rows === 0) {
-//            echo "No Applications<br>";
-//        } else {
-//            for ($row_index = 0; $row_index < $num_rows; $row_index++) {
-//                $result2->data_seek($row_index);
-//
-//
-//                $student = $result2->fetch_array(MYSQLI_ASSOC);
-//
-//
-//
-//
-//
-//
-//            }
-//        }
-//    }
 
-    
-    echo "success";
-
+/*
     function connectToDB($host, $user, $password, $database) {
         $db = mysqli_connect($host, $user, $password, $database);
         if (mysqli_connect_errno()) {
@@ -148,4 +159,6 @@
         }
         return $db;
     }
+*/
+    
 ?>
