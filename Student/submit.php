7ab555c0bc8966729e5e43ad33e14144b84e1294
Erik Ochoa <erikochoa26051@gmail.com>
2018-04-30 20:12:28 -0400

    changes to front end of student

--- Student/submit.php
+++ Student/submit.php
@@ -10,18 +10,41 @@
 	$body = <<<BODY
 	<br>
 	<h1 align="center">UMD CS TA Application</h1>
-	<h3> Are you an Undergraduate or Graduate student? </h3>
+	<h3 align="Center"> Are you an Undergraduate or Graduate student? </h3>
 	<form action="{$_SERVER["PHP_SELF"]}" method="post">
-	<div class="form-group">
-			<div class="col-sm-3 col-sm-push-1">
-					<input type="submit" class="btn btn-info" value="Undergraduate" name="undergraduate">
-			</div>
-			<div class="col-sm-3 col-sm-push-1">
-					<input type="submit" class="btn btn-info" value="Graduate" name="graduate">
-			</div>
-			<div class="col-sm-3 col-sm-push-1">
-					<input type="submit" class="btn btn-info" value="Main Menu" name="main">
-			</div>
+	<div class="form-group" align="center">
+		<div class="row slideanim">
+		  <div class="col-sm-2 col-xs-12">
+          </div>
+          <div class="col-sm-4 col-xs-12">
+            <div class="panel panel-default text-center">
+              <div class="panel-heading">
+                <h1>Undergraduate</h1>
+              </div>
+              <div class="panel-body">
+                <p>click below</p>
+              </div>
+              <div class="panel-footer">
+                <input type="submit" class="btn btn-info" value="Apply" name="undergraduate">
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4 col-xs-12">
+            <div class="panel panel-default text-center">
+              <div class="panel-heading">
+                <h1>Graduate</h1>
+              </div>
+              <div class="panel-body">
+                <p>click below</p>
+              </div>
+              <div class="panel-footer">
+                <input type="submit" class="btn btn-info" value="Apply" name="graduate">
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-2 col-xs-12">
+          </div>
+        </div>
 	</div>
 
 	</form>
--- Student/support.php
+++ Student/support.php
@@ -40,11 +40,18 @@ function generatePage($body, $title="Example") {
     </head>
 
     <body>
+        <br><img src="../Assets/umdLogo.gif" alt="UMD logo" style="margin-left: 10px"><br>
+        <hr style="height:1px;border:none;color:#C0C0C0;background-color:#C0C0C0;" />
 
         <div class="container-fluid">
           $body
         </div>
+        
         <br><hr style="height:1px;border:none;color:#C0C0C0;background-color:#C0C0C0;"/>
+        <form action = "../main.html" method='post' align="center">
+                <input type="submit" class="btn btn-info" name="home" value="Return Home">
+        </form>
+        <br><br>
 
         <script src="bootstrap/jquery-3.2.1.min.js"></script>
         <script src="bootstrap/js/bootstrap.min.js"></script>
