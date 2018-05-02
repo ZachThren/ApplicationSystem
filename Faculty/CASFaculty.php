0bc7521b5bb655e68eada1a6bc8500b4b023d64d
Mishel <xli12310@umd.edu>
2018-05-02 12:13:38 -0400

    cas

--- Faculty/CASFaculty.php
+++ Faculty/CASFaculty.php
@@ -1,10 +1,30 @@
 <?php
+require_once("support.php");
 
+$topPart = <<<EOPAGE
+
+  <form action="{$_SERVER['PHP_SELF']}" method="post">
+
+  <strong>DirectoryID: </strong><input type="text" name = "directoryid" class="form-control" /><br /><br />
+  <strong>Password: </strong><input type="password" name="password" class="form-control" /><br /><br>
+
+
+
+  <input type="reset" name="clear" class="btn btn-info"/>
+  <input type="submit" name="submit" class="btn btn-info"/>
+
+  </form>
+EOPAGE;
+
+$bottomPart = "";
+
+
+if (isset($_POST["submit"])){
 /* You need to update http.conf file so the ldap module is loaded */
 /* Entry in http.conf: LoadModule ldap_module modules/mod_ldap.so */
 
-$login_nm = $_POST["directoryid"];
-$login_passwd = $_POST["password"];
+$login_nm = trim($_POST["directoryid"]);
+$login_passwd = trim($_POST["password"]);
 
 /* Establish a connection to the LDAP server */
 $ldapconn=ldap_connect("ldap://ldap.umd.edu/",389) or die('Could not connect<br>');
@@ -19,13 +39,19 @@ $verify_user=ldap_bind($ldapconn,"uid=$login_nm,ou=people,dc=umd,dc=edu",$login_
 /* Returns 1 on Success */
 if ($verify_user != 1) {
   /* Failed */
-  echo "Invalid directoryId/password<br>";
+  $bottomPart .= "Invalid directoryId/password<br>";
 } else {
   /* Success */
-  echo "You have been authenticated as having a valid UMD directory ID.";
+  header("location: chooseTerm.php");
+
 }
 
 
 // Release connection
 ldap_unbind($ldapconn);
+}
+
+$body = $topPart.$bottomPart;
+echo generatePage($body);
+
 ?>
---
+++ Student/CASStudent.php
@@ -0,0 +1,57 @@
+<?php
+require_once("support.php");
+
+$topPart = <<<EOPAGE
+
+  <form action="{$_SERVER['PHP_SELF']}" method="post">
+
+  <strong>DirectoryID: </strong><input type="text" name = "directoryid" class="form-control" /><br /><br />
+  <strong>Password: </strong><input type="password" name="password" class="form-control" /><br /><br>
+
+
+
+  <input type="reset" name="clear" class="btn btn-info"/>
+  <input type="submit" name="submit" class="btn btn-info"/>
+
+  </form>
+EOPAGE;
+
+$bottomPart = "";
+
+
+if (isset($_POST["submit"])){
+/* You need to update http.conf file so the ldap module is loaded */
+/* Entry in http.conf: LoadModule ldap_module modules/mod_ldap.so */
+
+$login_nm = trim($_POST["directoryid"]);
+$login_passwd = trim($_POST["password"]);
+
+/* Establish a connection to the LDAP server */
+$ldapconn=ldap_connect("ldap://ldap.umd.edu/",389) or die('Could not connect<br>');
+// $ldapconn=ldap_connect("ldaps://ldap.umd.edu/",389) or die('Could not connect<br>');
+
+/* Set the protocol version to 3 (unless set to 3 by default) */
+ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+/* Bind user to LDAP with password */
+$verify_user=ldap_bind($ldapconn,"uid=$login_nm,ou=people,dc=umd,dc=edu",$login_passwd);
+
+/* Returns 1 on Success */
+if ($verify_user != 1) {
+  /* Failed */
+  $bottomPart .= "Invalid directoryId/password<br>";
+} else {
+  /* Success */
+  header("location: chooseTerm.php");
+
+}
+
+
+// Release connection
+ldap_unbind($ldapconn);
+}
+
+$body = $topPart.$bottomPart;
+echo generatePage($body);
+
+?>
