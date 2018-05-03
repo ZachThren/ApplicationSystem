4dbfdf234467f00b6e61a153278751e6d2e2a53a
nelsonle97 <nle@terpmail.umd.edu>
2018-05-03 09:58:56 -0400

    added favicons

--- Faculty/support.php
+++ Faculty/support.php
@@ -9,7 +9,15 @@
                 <title>$title</title>
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
                 <link rel="stylesheet" type="text/css" href="../Assets/mainstyle.css">
-
+                <link rel="apple-touch-icon" sizes="180x180" href="/Assets/apple-touch-icon.png">
+                <link rel="icon" type="image/png" sizes="32x32" href="/Assets/favicon-32x32.png">
+                <link rel="icon" type="image/png" sizes="16x16" href="/Assets/favicon-16x16.png">
+                <link rel="manifest" href="/Assets/site.webmanifest">
+                <link rel="mask-icon" href="/Assets/safari-pinned-tab.svg" color="#5bbad5">
+                <link rel="shortcut icon" href="/Assets/favicon.ico">
+                <meta name="msapplication-TileColor" content="#da532c">
+                <meta name="msapplication-config" content="/Assets/browserconfig.xml">
+                <meta name="theme-color" content="#ffffff">
             </head>
 
             <body>
